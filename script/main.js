25a016621ea5c6bd9f82bdb3e9fd511bcf4f3864
hoon <sugarcity07@gmail.com>
2025-11-10 15:14:38 +0900

    캐스퍼 추가 업포드

--- script/main.js
+++ script/main.js
@@ -65,34 +65,107 @@ $(document).ready(function () {
       </p>
     </div>
   </div> `;
-  
+
   $('body').append(modal);
   // 닫기 버튼 변수
   const c_btn = $('.modal #c_btn');
   let ch = $('#ch'); // 체크 박스 변수
 
   // 만약에 쿠키 파일이 존재 한다면 모달이 나오지 않게 한다. 
-    if($.cookie('popup')=='none'){
-      $('.modal').hide();
+  if ($.cookie('popup') == 'none') {
+    $('.modal').hide();
+  }
+
+  // 닫기 버튼 클릭시 체크 박스에 체크 여부를 판단하여 체크 되었으면 쿠기 생성
+  $('#ch, #c_btn').click(function () {
+    closePopup()
+  });
+
+  function closePopup() {
+    if (ch.is(':checked')) {  //체크 박스가 체크가 되면 쿠키를 생성하고 저장
+      $.cookie('popup', 'none', { expires: 1, path: '/' }); // 쿠키 파일 저장
+      $('.modal').fadeOut(); // 모달 윈도우 사라지게 하기
+    } else {
+
+      // 체크 박스에 체크를 하지 않은 경우 라면 그냥 모달 윈도를 닫는다. 
+      $('.modal').fadeOut();
     }
+  }
 
-    // 닫기 버튼 클릭시 체크 박스에 체크 여부를 판단하여 체크 되었으면 쿠기 생성
-    $('#ch, #c_btn').click(function(){
-      closePopup()
-    });
+  /* 윈도우 스크롤 이벤트  */
+  $(window).scroll(function () {
+    let sPos = $(this).scrollTop();  // 세로스크롤 값을 구함
+    console.log(sPos); // 콘솔모드에 출력
 
-    function closePopup(){
-      if(ch.is(':checked')){  //체크 박스가 체크가 되면 쿠키를 생성하고 저장
-        $.cookie('popup', 'none', {expires:1, path:'/'}); // 쿠키 파일 저장
-        $('.modal').fadeOut(); // 모달 윈도우 사라지게 하기
-      }else{"'"
-        // 체크 박스에 체크를 하지 않은 경우 라면 그냥 모달 윈도를 닫는다. 
-        $('.modal').fadeOut();
-      }
+    if (sPos >= 700) {
+      //3개 변수 값에 .h_txt_color서식을 제거한다.
+      gnb.addClass('h_txt_color')
+      //헤더에도 .h_on을 제거
+      $('header').addClass('h_on');
+      // 아이콘 색상 변경
+      h_icon.addClass('h_txt_color');
+      // 로고 이미지 속성 src값 변경
+      h1.attr('src', '../images/logo-casper_black.png');
+
+      $('header').mouseleave(function () {
+        //3개 변수 값에 .h_txt_color서식을 추가한다. 
+        gnb.addClass('h_txt_color');
+        //헤더에도 .h_on을 추가
+        $('header').addClass('h_on');
+        // 아이콘 색상 변경
+        h_icon.addClass('h_txt_color');
+        // 로고 이미지 속성 src값 변경
+        h1.attr('src', './images/logo-casper_black.png');
+      });
+
+    } else {
+      gnb.removeClass('h_txt_color');
+      $('header').removeClass('h_on');
+      h_icon.removeClass('h_txt_color');
+      h1.attr('src', './images/logo-casper-white.png');
+
+      $('header').mouseleave(function () {
+        //3개 변수 값에 .h_txt_color서식을 제거한다.
+        gnb.removeClass('h_txt_color')
+        //헤더에도 .h_on을 제거
+        $(this).removeClass('h_on');
+        // 아이콘 색상 변경
+        h_icon.removeClass('h_txt_color');
+        // 로고 이미지 속성 src값 변경
+        h1.attr('src', './images/logo-casper-white.png');
+
+      });
+
+
     }
+  });
 
 
 
 
 
+
+
+
+});      //제이쿼리
+
+$(function() {
+  var swiper = new Swiper('.top3', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+  });
 });
+
+
